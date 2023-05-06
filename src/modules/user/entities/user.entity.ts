0363821bd94b441de267063ca616d718debe18b4
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-06 19:18:17 +0200

    🗃️ Create User entity

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from '../enums/user-role.enum';
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
+
+    @Column({ name: 'email_address' })
+    emailAddress: string;
+
+    @Column({ name: 'first_name' })
+    firstName: string;
+
+    @Column({ name: 'last_name' })
+    lastName: string;
+
+    @Column({
+        name: 'last_verified_as_student',
+        type: 'timestamp with time zone',
+        nullable: true,
+    })
+    lastVerifiedAsStudent: Date | null;
+
+    @Column({
+        name: 'role',
+        enum: UserRole,
+        enumName: 'user_role',
+    })
+    role: UserRole;
+
+    @Column({
+        name: 'is_active',
+    })
+    isActive: boolean;
+}

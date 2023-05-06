9b1bbc47a4448b4a104d6d5b65f10d47f0f802c7
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-06 19:22:42 +0200

    ✨ Import TypeOrm- & UserModule in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+import { Credential } from './entities/credential.entity';
+import { IssuedRefreshToken } from './entities/issued-refresh-token.entity';
+import { OneTimeToken } from './entities/one-time-token.entity';
 
 @Module({
+    imports: [
+        UserModule,
+        TypeOrmModule.forFeature([
+            Credential,
+            IssuedRefreshToken,
+            OneTimeToken,
+        ]),
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })

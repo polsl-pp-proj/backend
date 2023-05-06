ae3b957dd5b9795631d83db50e511e7845b703f4
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-06 19:19:35 +0200

    🗃️ Create Auth-related entities

---
+++ src/modules/auth/entities/issued-refresh-token.entity.ts
@@ -0,0 +1,33 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity({ name: 'issued_refresh_tokens' })
+export class IssuedRefreshToken {
+    @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
+
+    @Column({ name: 'uuid' })
+    uuid: string;
+
+    @Column({ name: 'for_auth_token_uuid' })
+    forAuthTokenUuid: string;
+
+    @Column({ name: 'expiry', type: 'timestamp with time zone' })
+    expiry: Date;
+
+    @Column({ name: 'ip_address' })
+    ipAddress: string;
+
+    @Column({ name: 'user_id' })
+    userId: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn()
+    user: User;
+}

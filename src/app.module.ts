48eef5d1c14fd3a9bc0d5041a9fcd08233b12da2
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-06 13:30:59 +0200

    ✨ Import and configure TypeOrmModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [AuthModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            autoLoadEntities: true,
+            synchronize: false,
+        }),
+        AuthModule,
+    ],
     controllers: [AppController],
 })
 export class AppModule {}

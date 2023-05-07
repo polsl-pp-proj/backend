1c0896229dbfd59884cff4acccca49de1edf8ada
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-07 13:56:55 +0200

    ✨ Implement auth token-related methods

--- src/modules/auth/services/auth/auth.service.ts
+++ src/modules/auth/services/auth/auth.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { IAuthService } from 'src/interfaces/auth.service.interface';
+import { User } from 'src/modules/user/entities/user.entity';
+import { IssuedRefreshToken } from '../../entities/issued-refresh-token.entity';
+import { IAuthTokenService } from 'src/interfaces/auth-token.service.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService implements IAuthService {
+    /**
+     * Prepares AuthService object.
+     *
+     * @param authTokenService injected AuthTokenService object
+     */
+    constructor(private readonly authTokenService: IAuthTokenService) {}
+
+    async login(user: User, clientIP: string) {
+        return await this.authTokenService.createTokenPairForLogin(
+            user,
+            clientIP,
+        );
+    }
+
+    async refreshTokens(
+        issuedRefreshToken: IssuedRefreshToken,
+        clientIP: string,
+    ) {
+        return await this.authTokenService.createTokenPairForRefresh(
+            issuedRefreshToken,
+            clientIP,
+        );
+    }
+
+    async logout(issuedRefreshToken: IssuedRefreshToken) {
+        await this.authTokenService.invalidateTokenPair(issuedRefreshToken);
+    }
+
+    async logoutFromAllDevices(userId: number) {
+        await this.authTokenService.invalidateAllTokensForUser(userId);
+    }
+
+    requestPasswordReset(emailAddress: string): Promise<string> {
+        throw new Error('Method not implemented.');
+    }
+    resetPassword(
+        emailAddress: string,
+        oneTimeToken: string,
+        newPassword: string,
+    ): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    changePassword(userId: number, newPassword: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+}

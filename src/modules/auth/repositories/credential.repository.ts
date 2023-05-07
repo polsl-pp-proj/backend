fd1d66204b2462c81c9d0f46981a281dd295759c
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-07 14:36:20 +0200

    🗃️ Implement insertCredential method

--- src/modules/auth/repositories/credential.repository.ts
+++ src/modules/auth/repositories/credential.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Credential } from '../entities/credential.entity';
+import { CredentialType } from '../enums/credential-type.enum';
 
 @Injectable()
 export class CredentialRepository extends Repository<Credential> {
@@ -10,5 +11,22 @@ export class CredentialRepository extends Repository<Credential> {
         entityManager?: EntityManager,
     ) {
         super(Credential, entityManager ?? dataSource.createEntityManager());
+    }
+
+    async insertCredential({
+        userId,
+        type,
+        credential,
+    }: {
+        userId: number;
+        type: CredentialType;
+        credential: string;
+    }): Promise<Credential> {
+        const credentialEntity: Credential = this.create({
+            userId,
+            type,
+            credential,
+        });
+        return await this.save(credentialEntity, { reload: true });
     }
 }

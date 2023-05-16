de55ce752c2f20e17506f40679b20097cc700c63
npolak <nikoem.polak@itsilesia.com>
2023-05-16 13:01:33 +0200

    ♻️ Remove members field from CreateOrganzatioDto

--- src/modules/oragnization/dtos/create-organization.dto.ts
+++ src/modules/oragnization/dtos/create-organization.dto.ts
@@ -4,16 +4,14 @@ import {
     IsEmpty,
     IsNumber,
     IsString,
+    ValidateNested,
 } from 'class-validator';
 import { OrganizationMemberRole } from '../enums/organization-member-role.enum';
-import { AddMemberDto } from './add-member.dto';
+import { MemberDto } from './member.dto';
+import { Type } from 'class-transformer';
 
 export class CreateOrganizationDto {
     @IsString({ message: 'not_string' })
     @IsEmpty({ message: 'empty' })
     name: string;
-
-    @IsDefined({ message: 'not_defined' })
-    @IsArray({ message: 'not_array' })
-    members: AddMemberDto[];
 }

634a9a87677bbe18a2c58b911291b04cbedf7d1a
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-16 13:55:56 +0200

    🗃️ Set column names

--- src/modules/oragnization/entities/organization-user.entity.ts
+++ src/modules/oragnization/entities/organization-user.entity.ts
@@ -4,36 +4,28 @@ import {
     JoinColumn,
     ManyToOne,
     PrimaryGeneratedColumn,
-    Unique,
 } from 'typeorm';
 import { OrganizationMemberRole } from '../enums/organization-member-role.enum';
 import { Organization } from './organization.entity';
 import { User } from 'src/modules/user/entities/user.entity';
 
 @Entity({ name: 'organizations_users' })
-@Unique('UQ_organization_user', ['organizationId', 'userId']) //
 export class OrganizationUser {
     @PrimaryGeneratedColumn({ name: 'id' })
     id: number;
 
-    @Column({
-        foreignKeyConstraintName: 'FK_organization_user_organization_id',
-    })
+    @Column({ name: 'organization_id' })
     organizationId: number;
 
     @ManyToOne(() => Organization, { onDelete: 'CASCADE' })
-    @JoinColumn({
-        foreignKeyConstraintName: 'FK_organization_user_organization_id',
-    })
+    @JoinColumn({ name: 'organization_id' })
     organization: Organization;
 
-    @Column({ foreignKeyConstraintName: 'FK_organization_user_user_id' })
+    @Column({ name: 'user_id' })
     userId: number;
 
     @ManyToOne(() => User)
-    @JoinColumn({
-        foreignKeyConstraintName: 'FK_organization_user_user_id',
-    })
+    @JoinColumn({ name: 'user_id' })
     user: User;
 
     @Column({
@@ -41,6 +33,7 @@ export class OrganizationUser {
         enum: OrganizationMemberRole,
         enumName: 'organization_member_role',
         default: OrganizationMemberRole.Member,
+        name: 'role',
     })
     role: OrganizationMemberRole;
 }
--- src/modules/oragnization/entities/organization.entity.ts
+++ src/modules/oragnization/entities/organization.entity.ts
@@ -1,10 +1,4 @@
-import {
-    Column,
-    Entity,
-    OneToMany,
-    PrimaryGeneratedColumn,
-    Unique,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { OrganizationUser } from './organization-user.entity';
 
 @Entity({ name: 'organizations' })

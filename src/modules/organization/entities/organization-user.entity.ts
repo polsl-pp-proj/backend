5ba1166867ab0e44eaa2b0610da9acc3c560886f
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-21 16:17:39 +0200

    🗃️ Remove id from OrganizationUser entity

--- src/modules/organization/entities/organization-user.entity.ts
+++ src/modules/organization/entities/organization-user.entity.ts
@@ -1,27 +1,18 @@
-import {
-    Column,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { OrganizationMemberRole } from '../enums/organization-member-role.enum';
 import { Organization } from './organization.entity';
 import { User } from 'src/modules/user/entities/user.entity';
 
 @Entity({ name: 'organizations_users' })
 export class OrganizationUser {
-    @PrimaryGeneratedColumn({ name: 'id' })
-    id: number;
-
-    @Column({ name: 'organization_id' })
+    @PrimaryColumn({ name: 'organization_id' })
     organizationId: number;
 
     @ManyToOne(() => Organization, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'organization_id' })
     organization: Organization;
 
-    @Column({ name: 'user_id' })
+    @PrimaryColumn({ name: 'user_id' })
     userId: number;
 
     @ManyToOne(() => User)

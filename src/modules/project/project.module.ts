eb8a4416dd4d458c0178ed059f076bfef42e9586
npolak <nikoem.polak@itsilesia.com>
2023-05-22 18:08:33 +0200

    ✨ Create ProjectDraftSubmission entity

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { ProjectDraft } from './entities/project-draft.entity';
+import { ProjectDraftSubmission } from './entities/project-draft-submission.entity';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forFeature([
+            Project,
+            ProjectDraft,
+            ProjectDraftSubmission,
+        ]),
+    ],
     controllers: [],
     providers: [],
 })

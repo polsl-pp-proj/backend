e7bb0561c32978b96cf06f86f01d471fb77d977f
npolak <nikoem.polak@itsilesia.com>
2023-06-01 12:36:01 +0200

    ✨ Add endpoints declaration to ProjectController

--- src/modules/project/controllers/project/project/project.controller.ts
+++ src/modules/project/controllers/project/project/project.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    ValidationPipe,
+} from '@nestjs/common';
+import { validationConfig } from 'src/configs/validation.config';
+import { IProjectService } from 'src/interfaces/project.service.interface';
+import { UploadProjectDto } from 'src/modules/project/dtos/upload-project.dto';
 
 @Controller({ path: 'project', version: '1' })
-export class ProjectController {}
+export class ProjectController {
+    constructor(private readonly projectService: IProjectService) {}
+    @Get()
+    async getAllProjects() {}
+
+    @Get(':projectId')
+    async getProjectById(@Param('projectId', ParseIntPipe) projectId: number) {}
+
+    @Patch(':projectId')
+    async editProjectContent(
+        @Param('projectId', ParseIntPipe) projectId: number,
+        @Body(new ValidationPipe(validationConfig))
+        uploadProjectDto: UploadProjectDto,
+    ) {}
+}

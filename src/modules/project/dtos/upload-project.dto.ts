985528720be8f705c5a3f94d3c8e4d1d60199f7c
npolak <nikoem.polak@itsilesia.com>
2023-06-04 16:00:47 +0200

    ✨ Add ownerOrganizationId field

--- src/modules/project/dtos/upload-project.dto.ts
+++ src/modules/project/dtos/upload-project.dto.ts
@@ -1,10 +1,20 @@
-import { IsString, MaxLength, MinLength } from "class-validator";
+import {
+    IsDefined,
+    IsNumber,
+    IsString,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
 
 export class UploadProjectDto {
     @IsString()
     @MinLength(2, { message: 'too_short' })
     @MaxLength(60, { message: 'too_long' })
     name: string;
+
+    @IsDefined({ message: 'not_defined' })
+    @IsNumber({ maxDecimalPlaces: 0 }, { message: 'not_a_number' })
+    ownerOrganizationId: number;
 
     @IsString()
     @MinLength(2, { message: 'too_short' })
@@ -14,4 +24,7 @@ export class UploadProjectDto {
     @IsString()
     @MinLength(2, { message: 'too_short' })
     description: string;
+
+    @IsString()
+    fundingObjectives: string;
 }

666daa1d304cb1ee27079c5f2b11a72345732a63
npolak <nikoem.polak@itsilesia.com>
2023-06-04 15:58:48 +0200

    ✨ Create SimpleProjectDto

--- src/modules/project/dtos/project.dto.ts
+++ src/modules/project/dtos/project.dto.ts
@@ -3,10 +3,24 @@ export class ProjectDto {
     name: string;
     shortDescription: string;
     description: string;
+    fundingObjectives: string;
+    organizationName: string;
     // TO DO
     // Add assets, orgnization name
 
     constructor(partialProjectDto: Partial<ProjectDto>) {
         Object.assign(this, partialProjectDto);
+    }
+}
+
+export class SimpleProjectDto {
+    id: number;
+    name: string;
+    shortDescription: string;
+    organizationName: string;
+    // Add thumbnail
+
+    constructor(partialSimpleProjectDto: Partial<SimpleProjectDto>) {
+        Object.assign(this, partialSimpleProjectDto);
     }
 }

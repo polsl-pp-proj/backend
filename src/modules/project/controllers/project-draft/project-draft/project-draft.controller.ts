73f1a4e0d1e9537a0239a8aac21edc66e8cf48a8
npolak <nikoem.polak@itsilesia.com>
2023-06-04 15:54:36 +0200

    ✨ Implement ProjectDraftControllers methods

--- src/modules/project/controllers/project-draft/project-draft/project-draft.controller.ts
+++ src/modules/project/controllers/project-draft/project-draft/project-draft.controller.ts
@@ -1,4 +1,83 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    ValidationPipe,
+} from '@nestjs/common';
+import { validationConfig } from 'src/configs/validation.config';
+import { RecordNotFoundException } from 'src/exceptions/record-not-found.exception';
+import { IProjectService } from 'src/interfaces/project.service.interface';
+import { AuthTokenPayload } from 'src/modules/auth/decorators/param/user.decorator';
+import { AuthTokenPayloadDto } from 'src/modules/auth/dtos/auth-token-payload.dto';
+import {
+    ProjectDraftDto,
+    SimpleProjectDraftDto,
+} from 'src/modules/project/dtos/project-draft.dto';
+import { UploadProjectDto } from 'src/modules/project/dtos/upload-project.dto';
 
 @Controller({ path: 'project/draft', version: '1' })
-export class ProjectDraftController {}
+export class ProjectDraftController {
+    constructor(private readonly projectService: IProjectService) {}
+
+    @Get('organization/:organizationId')
+    async getAllOrganizationDrafts(
+        @Param('organizationId', ParseIntPipe) organizationId: number,
+    ): Promise<SimpleProjectDraftDto[]> {
+        return await this.projectService.getAllOrganizationsDrafts(
+            organizationId,
+        );
+    }
+
+    @Get(':draftId')
+    async getProjectDraft(
+        @Param('draftId', ParseIntPipe) draftId: number,
+        @AuthTokenPayload() user: AuthTokenPayloadDto,
+    ): Promise<ProjectDraftDto> {
+        try {
+            return await this.projectService.getDraftById(draftId, user.userId);
+        } catch (ex) {
+            if (ex instanceof RecordNotFoundException) {
+                throw new NotFoundException(ex.message);
+            }
+            throw ex;
+        }
+    }
+
+    @Post()
+    async createProjectDraft(
+        @Body(new ValidationPipe(validationConfig))
+        uploadProjectDto: UploadProjectDto,
+        @AuthTokenPayload() user: AuthTokenPayloadDto,
+    ): Promise<void> {
+        await this.projectService.createProjectDraft(
+            uploadProjectDto,
+            user.userId,
+        );
+    }
+
+    @Patch(':draftId')
+    async editProjectDraft(
+        @Param('draftId', ParseIntPipe) draftId: number,
+        @Body(new ValidationPipe(validationConfig))
+        uploadProjectDto: UploadProjectDto,
+        @AuthTokenPayload() user: AuthTokenPayloadDto,
+    ): Promise<void> {
+        try {
+            await this.projectService.updateProjectDraft(
+                draftId,
+                uploadProjectDto,
+                user.userId,
+            );
+        } catch (ex) {
+            if (ex instanceof RecordNotFoundException) {
+                throw new NotFoundException(ex.message);
+            }
+            throw ex;
+        }
+    }
+}

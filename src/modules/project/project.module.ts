3d70cade9d9ef92d4d261a60829e941564d2ebaa
npolak <nikoem.polak@itsilesia.com>
2023-06-04 16:09:54 +0200

    ➕ Add controllers and provider to ProjectModule

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -3,16 +3,42 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { ProjectDraft } from './entities/project-draft.entity';
 import { ProjectDraftSubmission } from './entities/project-draft-submission.entity';
+import { UserModule } from '../user/user.module';
+import { OrganizationModule } from '../organization/organization.module';
+import { ProjectService } from './services/project/project.service';
+import { IProjectService } from 'src/interfaces/project.service.interface';
+import { IProjectDraftSubmissionService } from 'src/interfaces/project-draft-submission.service.interface';
+import { ProjectDraftRepository } from './repositories/project-draft.repositry';
+import { ProjectDraftSubmissionRepository } from './repositories/project-draft-submission.repository';
+import { ProjectRepository } from './repositories/project.repository';
+import { ProjectController } from './controllers/project/project/project.controller';
+import { ProjectDraftController } from './controllers/project-draft/project-draft/project-draft.controller';
+import { ProjectDraftSubmissionController } from './controllers/project-draft-submission/project-draft-submission/project-draft-submission.controller';
 
 @Module({
     imports: [
+        UserModule,
+        OrganizationModule,
         TypeOrmModule.forFeature([
             Project,
             ProjectDraft,
             ProjectDraftSubmission,
         ]),
     ],
-    controllers: [],
-    providers: [],
+    controllers: [
+        ProjectController,
+        ProjectDraftController,
+        ProjectDraftSubmissionController,
+    ],
+    providers: [
+        { provide: IProjectService, useClass: ProjectService },
+        {
+            provide: IProjectDraftSubmissionService,
+            useClass: ProjectDraftSubmission,
+        },
+        ProjectDraftRepository,
+        ProjectDraftSubmissionRepository,
+        ProjectRepository,
+    ],
 })
 export class ProjectModule {}

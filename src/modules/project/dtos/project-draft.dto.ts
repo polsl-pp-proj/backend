9692dc69e96bcb62defbfe7fb130df672cdfe0fd
npolak <nikoem.polak@itsilesia.com>
2023-06-04 15:57:56 +0200

    ✨ Add lastModified field to ProjectDraftDto

--- src/modules/project/dtos/project-draft.dto.ts
+++ src/modules/project/dtos/project-draft.dto.ts
@@ -1,7 +1,10 @@
-import { ProjectDto } from './project.dto';
+import { ProjectDto, SimpleProjectDto } from './project.dto';
 
 export class ProjectDraftDto extends ProjectDto {
+    lastModified: Date;
     constructor(partialProjectDraftDto: Partial<ProjectDraftDto>) {
         super(partialProjectDraftDto);
     }
 }
+
+export class SimpleProjectDraftDto extends SimpleProjectDto {}

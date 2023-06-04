d602461d19d553f2ef5bea6ab0480e65af1f74c2
npolak <nikoem.polak@itsilesia.com>
2023-06-04 15:52:29 +0200

    ✨ Add getSubmissionById method

--- src/interfaces/project-draft-submission.service.interface.ts
+++ src/interfaces/project-draft-submission.service.interface.ts
@@ -1,14 +1,9 @@
-import { ProjectDraftDto } from 'src/modules/project/dtos/project-draft.dto';
 import { SubmissionDto } from 'src/modules/project/dtos/submission.dto';
 
 export abstract class IProjectDraftSubmissionService {
-    abstract getPublicationSubmissions(): Promise<SubmissionDto[]>;
+    abstract getSubmissions(): Promise<SubmissionDto[]>;
 
-    abstract getChangeSubmissions(): Promise<SubmissionDto[]>;
-
-    abstract getSubmssionProjectDraft(
-        submissinId: number,
-    ): Promise<ProjectDraftDto>;
+    abstract getSubmissionById(submissionId: number): Promise<SubmissionDto>;
 
     abstract rejectSubmission(
         submissionId: number,

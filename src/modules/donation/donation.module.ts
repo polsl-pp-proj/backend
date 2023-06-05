506f1b435152ed8037293eb8532ea8d87cf8ce4e
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-05 18:58:01 +0200

    ✨ Add necessary imports and exports

--- src/modules/donation/donation.module.ts
+++ src/modules/donation/donation.module.ts
@@ -3,12 +3,17 @@ import { DonationService } from './services/donation/donation.service';
 import { DonationController } from './controller/donation/donation.controller';
 import Stripe from 'stripe';
 import * as dotenv from 'dotenv';
+import { ProjectDonationRepository } from './repositories/project-donation.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectDonation } from './entities/project-donation.entity';
 
 dotenv.config();
 
 @Module({
+    imports: [TypeOrmModule.forFeature([ProjectDonation])],
     providers: [
         DonationService,
+        ProjectDonationRepository,
         {
             provide: 'STRIPE',
             useValue: new Stripe(process.env.STRIPE_SECRET_KEY, {
@@ -19,5 +24,6 @@ dotenv.config();
         },
     ],
     controllers: [DonationController],
+    exports: ['STRIPE'],
 })
 export class DonationModule {}

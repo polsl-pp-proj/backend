eda5f646f7be036d1875c6b4eb5bd932c0a69777
npolak <nikoem.polak@itsilesia.com>
2023-06-08 14:53:09 +0200

    ✨ Create ProjectDraftOpenPositon entity

---
+++ src/modules/project/entities/project-draft-open-position.entity.ts
@@ -0,0 +1,30 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ProjectDraft } from './project-draft.entity';
+
+@Entity({ name: 'project_draft_open_positions' })
+export class ProjectDraftOpenPosition {
+    @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
+
+    @Column({ name: 'project_draft_id' })
+    projectDraftId: number;
+
+    @ManyToOne(() => ProjectDraft)
+    @JoinColumn({ name: 'draft_id' })
+    projectDraft: ProjectDraft;
+
+    @Column({ name: 'name' })
+    name: string;
+
+    @Column({ name: 'description' })
+    description: string;
+
+    @Column('jsonb', { name: 'requirements', default: {} })
+    requirements: string;
+}

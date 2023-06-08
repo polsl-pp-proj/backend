6c8d1d4c96f7283eb6b2ed1ec9cd7b7529350faf
npolak <nikoem.polak@itsilesia.com>
2023-06-08 14:52:43 +0200

    ✨ Create ProjectOpenPosition entity

---
+++ src/modules/project/entities/project-open-position.entity.ts
@@ -0,0 +1,32 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Project } from './project.entity';
+
+@Entity({ name: 'project_open_positions' })
+export class ProjectOpenPosition {
+    @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
+
+    @Column({ name: 'project_id' })
+    projectId: number;
+
+    @ManyToOne(() => Project, (project) => project.openPositions, {
+        cascade: true,
+    })
+    @JoinColumn({ name: 'project_id' })
+    project: Project;
+
+    @Column({ name: 'name' })
+    name: string;
+
+    @Column({ name: 'description' })
+    description: string;
+
+    @Column({ name: 'requirements', default: [], type: 'jsonb' })
+    requirements: string[];
+}

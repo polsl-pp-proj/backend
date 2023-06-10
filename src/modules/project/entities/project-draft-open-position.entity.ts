26f5c415a1717d8b2758e17ded8de8ea8b511eea
npolak <nikoem.polak@itsilesia.com>
2023-06-10 19:20:22 +0200

    ✨ Add timestamps to ProjectDraftOpenPosition entity

--- src/modules/project/entities/project-draft-open-position.entity.ts
+++ src/modules/project/entities/project-draft-open-position.entity.ts
@@ -27,4 +27,10 @@ export class ProjectDraftOpenPosition {
 
     @Column('jsonb', { name: 'requirements', default: {} })
     requirements: string;
+
+    @Column({ name: 'created_at' })
+    createdAt: Date;
+
+    @Column({ name: 'updated_at' })
+    updatedAt: Date;
 }

928118af1052be9ac5741b44630b17fb01c5c813
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-13 13:29:18 +0200

    ✨ Import RedisPubSubModule

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -6,10 +6,15 @@ import { UserNotification } from './entities/user-notification.entity';
 import { NotificationController } from './controllers/notification/notification.controller';
 import { OrganizationNotificationRepository } from './repositories/organization-notification.repository';
 import { UserNotificationRepository } from './repositories/user-notification.repository';
+import { RedisPubSubModule } from 'nest-pubsub';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([OrganizationNotification, UserNotification]),
+        RedisPubSubModule.register({
+            path: process.env.REDIS_SOCK,
+            password: process.env.REDIS_PASS,
+        }),
     ],
     providers: [
         NotificationService,

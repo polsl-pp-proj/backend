32a16558de829a43de55ebfa74fe1a4a1b8d7341
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-13 13:26:31 +0200

    ✨ Create notification interface

---
+++ src/modules/notification/interfaces/notification.interface.ts
@@ -0,0 +1,13 @@
+import { NotificationType } from '../enums/notification-type.enum';
+
+export interface INotification {
+    id: number;
+    subject: string;
+    message: string;
+    projectId: number;
+    organizationName: string;
+    type: NotificationType;
+    seen: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}

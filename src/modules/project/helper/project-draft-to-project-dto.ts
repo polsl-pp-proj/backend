a6148f562dbb6599cdeed4ef157e19b4e3853531
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-16 20:46:12 +0200

    ✨ Add assets to Simple- & ProjectDto

--- src/modules/project/helper/project-draft-to-project-dto.ts
+++ src/modules/project/helper/project-draft-to-project-dto.ts
@@ -1,4 +1,4 @@
-import { OpenPositionDto } from '../dtos/open-position.dto';
+import { AssetDto } from 'src/modules/asset/dtos/asset.dto';
 import { ProjectDto, SimpleProjectDto } from '../dtos/project.dto';
 import { ProjectDraft } from '../entities/project-draft.entity';
 import { convertProjectDraftOpenPositionToOpenPositionDto } from './project-open-position-to-open-position-dto';
@@ -15,6 +15,14 @@ export const convertProjectDraftToProjectDto = (projectDraft: ProjectDraft) => {
         openPositions: projectDraft.openPositions.map((openPosition) =>
             convertProjectDraftOpenPositionToOpenPositionDto(openPosition),
         ),
+        assets: projectDraft.galleryEntries.map(
+            (galleryEntry) =>
+                new AssetDto({
+                    title: galleryEntry.asset.title,
+                    url: galleryEntry.asset.url,
+                    type: galleryEntry.asset.type,
+                }),
+        ),
         createdAt: projectDraft.createdAt.valueOf(),
         updatedAt: projectDraft.updatedAt.valueOf(),
     });
@@ -23,6 +31,7 @@ export const convertProjectDraftToProjectDto = (projectDraft: ProjectDraft) => {
 export const convertProjectDraftToSimpleProjectDto = (
     projectDraft: ProjectDraft,
 ) => {
+    const firstAsset = projectDraft.galleryEntries[0].asset;
     return new SimpleProjectDto({
         id: projectDraft.id,
         name: projectDraft.name,
@@ -31,5 +40,10 @@ export const convertProjectDraftToSimpleProjectDto = (
         organizationName: projectDraft.ownerOrganization.name,
         createdAt: projectDraft.createdAt.valueOf(),
         updatedAt: projectDraft.updatedAt.valueOf(),
+        thumbnail: new AssetDto({
+            title: firstAsset.title,
+            url: firstAsset.url,
+            type: firstAsset.type,
+        }),
     });
 };

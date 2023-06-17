8c1ff8ba75df878b0ec2527532ad14506636c212
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-17 11:21:18 +0200

    🚚 Rename CreateCategoryDto to CategoryNameDto

--- src/modules/category/dtos/category-name.dto.ts
+++ src/modules/category/dtos/category-name.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
-export class CreateCategoryDto {
+export class CategoryNameDto {
     @IsString({ message: 'not_string' })
     @MinLength(3, { message: 'name_too_short' })
     @MaxLength(50, { message: 'name_too_long' })

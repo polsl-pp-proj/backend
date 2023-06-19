3d36c44d3177533d9e039fa911d110a76d7da5b9
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-19 13:55:35 +0200

    🐛 Remove id from Project(Draft)Category entity

--- src/modules/project/entities/project-draft-category.entity.ts
+++ src/modules/project/entities/project-draft-category.entity.ts
@@ -1,10 +1,10 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { ProjectDraft } from './project-draft.entity';
 import { ProjectCategoryBase } from './project-category.entitybase';
 
 @Entity({ name: 'project_draft_categories' })
 export class ProjectDraftCategory extends ProjectCategoryBase {
-    @Column({ name: 'project_draft_id' })
+    @PrimaryColumn({ name: 'project_draft_id' })
     projectDraftId: number;
 
     @ManyToOne(() => ProjectDraft, { onDelete: 'CASCADE' })

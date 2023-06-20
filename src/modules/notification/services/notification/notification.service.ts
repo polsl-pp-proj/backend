420e3dedda7b56f67ee5f2eb13d972016c4e95fd
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-20 14:33:38 +0200

    🚑️ Fix access to null properties

--- src/modules/notification/services/notification/notification.service.ts
+++ src/modules/notification/services/notification/notification.service.ts
@@ -372,7 +372,8 @@ export class NotificationService {
 
                 await this.sendNotificationCreated(
                     'org',
-                    notification.project.projectDraft.ownerOrganization.id,
+                    notification.project.projectDraft.ownerOrganization.id ??
+                        notification.projectDraft.ownerOrganization.id,
                     new NotificationDto({
                         id: notification.id,
                         subject: notification.subject,
